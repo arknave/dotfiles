affed260879bcd8fde4186c3aca72d9f90615d12
Arnav Sastry <arnavsastry@gmail.com>
2015-08-31 13:38:42 -0500

    multiple files?

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,23 +1,11 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(tool-bar-mode nil))
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
 ;; Use the saves directory for the backup ~ files
 (setq backup-directory-alist `(("." . "~/.saves")))
 
-;; Disable tabs, 4 spaces ftw
+;; Disable tabs, 4 spaces
 (setq-default c-basic-offset 4
 	      tab-width 4
 	      indent-tabs-mode nil)
 
 (set-face-attribute 'default t :font "Source Code Pro for Powerline-14")
+
+(load "~/.emacs.d/my-noexternals.el")
---
+++ emacs/.emacs.d/my-noexternals.el
@@ -0,0 +1,1 @@
+(when (fboundp 'tool-bar-mode) (tool-bar-mode nil))
